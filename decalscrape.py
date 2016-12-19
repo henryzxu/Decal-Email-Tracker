19f3d3a5d052c5af8120e41ec7d81ea8066c2641
Henry <henryxu@berkeley.edu>
2016-12-18 21:54:17 -0800

    Update decalscrape.py
    Formatting for clarity

--- decalscrape.py
+++ decalscrape.py
@@ -1,34 +1,35 @@
 import urllib.request, re
 
+# Find Decal urls
 with urllib.request.urlopen('http://www.decal.org/courses') as response:
     html = response.read()
 html = str(html)
 z = re.findall('href=.(\d\d\d\d).', html)
 
-# y = []
-# for url in z:
-#     with urllib.request.urlopen('http://www.decal.org/courses/'+url) as response:
-#         html = response.read()
-#     html = str(html)
-#     y += [re.search(r'Course Contact:</strong> ([\w .()-]+)', html).group()]
-
-# j = []
-# for email in y:
-#     k = re.sub('\w+(.AT.)\w+', '@', email)
-#     j += [k]
-# file = open('Decal Emails','w')
-# for email in j:
-#     file.write(email + '\n')
-# file.close()
-
-
-
+# Write obtained emails to a local file
+y = []
 for url in z:
-    with urllib.request.urlopen('http://www.decal.org/courses/' + url) as response:
+    with urllib.request.urlopen('http://www.decal.org/courses/'+url) as response:
         html = response.read()
     html = str(html)
-    y = re.search(r'<h2>([\w :]+)</h2>',html)
-    if y:
-        print(y.group(1))
-    else:
-        print('\n')
+    y += [re.search(r'Course Contact:</strong> ([\w .()-]+)', html).group()]
+j = []
+for email in y:
+    k = re.sub('\w+(.AT.)\w+', '@', email) # Email formatting
+    j += [k]
+file = open('Decal Emails','w')
+for email in j:
+    file.write(email + '\n')
+file.close()
+
+# (Alternative) Print obtained emails 
+# for url in z:
+    # with urllib.request.urlopen('http://www.decal.org/courses/' + url) as response:
+        # html = response.read()
+    # html = str(html)
+    # y = re.search(r'<h2>([\w :]+)</h2>',html)
+    # if y:
+        # print(y.group(1))
+    # else:
+        # print('\n')
+
